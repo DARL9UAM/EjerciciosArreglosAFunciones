6a80079b8d4fbad0830dce9a8e53830306d4c47e
Jonathan Josué Rivera Guido <164054777+jriver123@users.noreply.github.com>
2024-10-20 17:45:05 -0700

    Ejercicio #5 Arreglo Enteros

---
+++ ArregloEnteros/InvertirYcontar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArregloEnteros
+
+{
+        //definicion de la clase interna
+        internal class InvertirYcontar
+        {
+        //recibe un arreglo de enteros y lo devuelve invertido
+        public static int[] InvertirArreglo(int[] array)
+            {
+                int[] invertido = new int[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    invertido[i] = array[array.Length - 1 - i]; //asignacion de la posicion
+                }
+                return invertido;  //devuelve el arreglo invertido
+        }
+
+            public static void ImprimirArreglo(int[] array)
+        {
+                foreach (int num in array) //Inicia un bucle foreach que itera sobre cada elemento num en el arreglo array.
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+            public static int ContarImpares(int[] array)
+            {
+                int contador = 0;
+                foreach (int num in array)
+                {
+                    if (num % 2 != 0) //comprabacion si es impar
+                    {
+                        contador++; //incremento del contador si es impar
+                }
+                }
+                return contador; //devuelve el contador
+            }
+}
+    }
+
---
+++ ArregloEnteros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArregloEnteros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Imprime un mensaje pidiendo al usuario que ingrese los datos del arreglo
+            Console.WriteLine("Ingrese los elementos del arreglo separados por espacios:");
+
+            //lectura de la variable y la almacena
+            string input = Console.ReadLine();
+
+            //Convierte la cadena de entrada en un arreglo de enteros
+            int[] array = input.Split(' ').Select(int.Parse).ToArray();
+
+            //Imprime el arreglo original
+            Console.WriteLine("Arreglo original:");
+            //llama al metodo imprimir arreglo y le pasa el arreglo original
+            InvertirYcontar.ImprimirArreglo(array);
+
+            //llama al metodo invertirarreglo de la clase invertir y contar para invertir el arreglo original y lo almacena
+            //en el array invertido
+            int[] arrayInvertido = InvertirYcontar.InvertirArreglo(array);
+
+            //se muestra el arreglo invertido gracias al metodo imprimir arreglo establecido en la clase invertir y contar
+            Console.WriteLine("Arreglo invertido:");
+            InvertirYcontar.ImprimirArreglo(arrayInvertido);
+
+            //Llama al método ContarImpares de la clase InvertirYcontar para contar
+            // los números impares en el arreglo invertido y almacena el resultado en la variable impares.
+            int impares = InvertirYcontar.ContarImpares(arrayInvertido);
+            Console.WriteLine($"Cantidad de valores impares en el arreglo invertido: {impares}");
+
+            Console.ReadKey();
+        }
+    }
+}
